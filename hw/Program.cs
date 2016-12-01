31021fa9849680890f2351232f20112cecc6840e
cosmicy <cosmicy@outlook.com>
2016-12-01 17:09:09 +0800

    第一个hw

---
+++ hw/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args.Length == 1)
+            {
+                bool b = Regex.IsMatch(args[0], @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+
+                Console.WriteLine(b);
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
